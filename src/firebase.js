87c299c41d3528f9b0d5b1e169008ed65c45efaa
Ankur Prabhu <85862184+AnkurPrabhu@users.noreply.github.com>
2023-10-19 19:22:11 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDaDjYKPK_rKBh9T4aSrTmwSq2QXfzdqwk",
-  authDomain: "pdf-manager-99c0d.firebaseapp.com",
-  projectId: "pdf-manager-99c0d",
-  storageBucket: "pdf-manager-99c0d.appspot.com",
-  messagingSenderId: "654912100720",
-  appId: "1:654912100720:web:4dd3daa51009f55e2a97b5",
-  measurementId: "G-V1JZM6TL0Q",
+  <your api key>
 };
 
 // Initialize Firebase
