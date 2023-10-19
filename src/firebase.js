40aae421c69e0f6321ceaadc456a6a1c101dfd87
Ankur Prabhu <85862184+AnkurPrabhu@users.noreply.github.com>
2023-10-19 19:22:23 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  <your api key>
+ // <your api key>
 };
 
 // Initialize Firebase
